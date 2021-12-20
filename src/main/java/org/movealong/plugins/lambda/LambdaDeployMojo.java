07da61828c178c769e259f160e127822391053f8
Nate Riffe <inkblot@movealong.org>
2021-12-20 08:07:14 -0600

    Let functionArn be null
    Relax the requirement for a functionArn in order to better facilitate initial deployment, so that an artifact can be uploaded in anticipation of defining a function.

--- src/main/java/org/movealong/plugins/lambda/LambdaDeployMojo.java
+++ src/main/java/org/movealong/plugins/lambda/LambdaDeployMojo.java
@@ -10,10 +10,10 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.*;
 import com.jnape.palatable.lambda.adt.Either;
+import com.jnape.palatable.lambda.adt.Maybe;
 import com.jnape.palatable.lambda.adt.hlist.Tuple2;
 import com.jnape.palatable.lambda.adt.hlist.Tuple3;
 import com.jnape.palatable.lambda.functions.Fn1;
-import com.jnape.palatable.lambda.functions.builtin.fn1.Id;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.apache.maven.plugin.AbstractMojo;
@@ -24,14 +24,16 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.function.Function;
 
 import static com.jnape.palatable.lambda.adt.Either.trying;
 import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.maybe;
 import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Find.find;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Map.map;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.ToCollection.toCollection;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Tupler2.tupler;
 import static java.lang.String.format;
 import static org.movealong.plugins.lambda.PartNumber.partNumbers;
 
@@ -43,7 +45,7 @@ public class LambdaDeployMojo extends AbstractMojo {
     @Parameter(required = true)
     private String region;
 
-    @Parameter(required = true)
+    @Parameter
     private String functionArn;
 
     @Parameter(required = true)
@@ -62,10 +64,11 @@ public class LambdaDeployMojo extends AbstractMojo {
     public void execute() throws MojoFailureException {
         getLog().info(format("Successfully updated function %s",
                              fileExists(getCodeBundle())
-                                     .flatMap(functionExists(getRegion(), getFunctionArn()))
+                                     .flatMap(functionExists(getRegion(), maybe(getFunctionArn())))
                                      .flatMap(uploadBundle(getLog(), getRegion(), getS3Bucket(), getS3Prefix()))
                                      .flatMap(updateFunctionCode(getRegion(), getS3Bucket(), isPublish()))
-                                     .fmap(UpdateFunctionCodeResult::getFunctionArn)
+                                     .fmap(m -> m.match(constantly("N/A"),
+                                                        UpdateFunctionCodeResult::getFunctionArn))
                                      .orThrow(MojoFailureException::new)));
     }
 
@@ -74,18 +77,21 @@ public class LambdaDeployMojo extends AbstractMojo {
                                .toEither(() -> format("Code bundle not found: %s", codeBundle.getAbsolutePath()));
     }
 
-    private static <T> Fn1<T, Either<String, Tuple2<T, String>>>
-    functionExists(String region, String functionArn) {
+    private static <T> Fn1<T, Either<String, Tuple2<T, Maybe<String>>>>
+    functionExists(String region, Maybe<String> mFunctionArn) {
         return (T t) -> withLambdaClient(
                 region,
-                client -> find(functionConfig -> functionConfig.getFunctionArn().equals(functionArn),
-                               client.listFunctions().getFunctions())
-                        .fmap(FunctionConfiguration::getFunctionName)
-                        .fmap(functionName -> tuple(t, functionName))
-                        .toEither(() -> format("Function not found: %s", functionArn)));
+                client -> mFunctionArn
+                        .<String, Either<String, ?>, Maybe<String>, Either<String, Maybe<String>>>traverse(
+                                functionArn -> find(functionConfig -> functionConfig.getFunctionArn().equals(functionArn),
+                                                    client.listFunctions().getFunctions())
+                                        .fmap(FunctionConfiguration::getFunctionName)
+                                        .toEither(() -> format("Function not found: %s", functionArn)),
+                                Either::right))
+                .fmap(tupler(t));
     }
 
-    private static Fn1<Tuple2<File, String>, Either<String, Tuple3<String, String, CompleteMultipartUploadResult>>>
+    private static <T> Fn1<Tuple2<File, T>, Either<String, Tuple3<String, T, CompleteMultipartUploadResult>>>
     uploadBundle(Log log, String region, String bucket, String prefix) {
         return t -> t.into((codeBundle, functionName) -> withS3Client(region, client -> trying(() -> {
             String key = prefix + "/" + codeBundle.getName();
@@ -94,9 +100,11 @@ public class LambdaDeployMojo extends AbstractMojo {
             String uploadId =
                     client.initiateMultipartUpload(new InitiateMultipartUploadRequest(bucket, key))
                           .getUploadId();
-            return tuple(key, functionName, client.completeMultipartUpload(
-                    new CompleteMultipartUploadRequest(bucket, key, uploadId, Id.<Iterable<PartNumber>>id()
-                            .fmap(map(createUploadPartRequest(bucket, codeBundle, key, uploadId)))
+            return tuple(key, functionName, client.completeMultipartUpload(new CompleteMultipartUploadRequest(
+                    bucket,
+                    key,
+                    uploadId,
+                    map(createUploadPartRequest(bucket, codeBundle, key, uploadId))
                             .fmap(map(client::uploadPart))
                             .fmap(map(UploadPartResult::getPartETag))
                             .fmap(toCollection(ArrayList::new))
@@ -117,20 +125,23 @@ public class LambdaDeployMojo extends AbstractMojo {
                 .withFile(codeBundle);
     }
 
-    private static Fn1<Tuple3<String, String, CompleteMultipartUploadResult>, Either<String, UpdateFunctionCodeResult>>
+    private static Fn1<Tuple3<String, Maybe<String>, CompleteMultipartUploadResult>, Either<String, Maybe<UpdateFunctionCodeResult>>>
     updateFunctionCode(String region, String bucket, boolean publish) {
-        return t -> t.into((key, functionName, uploadResult) -> trying(() -> withLambdaClient(
-                region,
-                lambda -> lambda.updateFunctionCode(
-                        new UpdateFunctionCodeRequest()
-                                .withFunctionName(functionName)
-                                .withS3Bucket(bucket)
-                                .withS3Key(key)
-                                .withPublish(publish))))
-                .biMapL(Throwable::getMessage));
+        return t -> t.into((key, mFunctionName, uploadResult) -> mFunctionName
+                .<UpdateFunctionCodeResult, Either<String, ?>, Maybe<UpdateFunctionCodeResult>, Either<String, Maybe<UpdateFunctionCodeResult>>>traverse(
+                        functionName -> trying(() -> withLambdaClient(
+                                region,
+                                lambda -> lambda.updateFunctionCode(
+                                        new UpdateFunctionCodeRequest()
+                                                .withFunctionName(functionName)
+                                                .withS3Bucket(bucket)
+                                                .withS3Key(key)
+                                                .withPublish(publish))))
+                                .biMapL(Throwable::getMessage),
+                        Either::right));
     }
 
-    private static <T> T withLambdaClient(String region, Function<AWSLambda, T> fn) {
+    private static <T> T withLambdaClient(String region, Fn1<AWSLambda, T> fn) {
         AWSLambda lambdaClient = AWSLambdaClientBuilder
                 .standard()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
@@ -143,7 +154,7 @@ public class LambdaDeployMojo extends AbstractMojo {
         }
     }
 
-    private static <T> T withS3Client(String region, Function<AmazonS3, T> fn) {
+    private static <T> T withS3Client(String region, Fn1<AmazonS3, T> fn) {
         AmazonS3 s3Client = AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
